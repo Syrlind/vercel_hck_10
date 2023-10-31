5cf7567351d56b15cc8407937458e042e99b47fa
Stanislaus Kanopi Johan Trielianto <148022079+Syrlind@users.noreply.github.com>
2023-10-31 14:37:38 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,24 @@
+from email import message 
+from fastapi import FastAPI,Header ,HTTPException
+
+app =FastAPI()
+#key yang diperlukan
+key = "hacktiv8mania2023"
+
+@app.get("/")
+def helloFunction():
+    return {
+        "message":"hello from the other side "
+
+    }
+
+@app.get("/secret")
+def helloFunction(api_key : str =Header(None)):
+    # check api_key dari header 
+    if api_key is None or api_key != key : 
+        raise HTTPException(status_code=401,detail="Invalid API Key")
+    
+    return {
+
+        "message":"secret message  "
+        }
